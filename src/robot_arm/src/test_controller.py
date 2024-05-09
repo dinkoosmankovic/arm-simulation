1f14e4c6dcec721de9b11d1baea9701585ecf731
Dinko Osmankovic <dinko.osmankovic@etf.unsa.ba>
2024-05-09 17:16:25 +0000

    Working prototype.

--- src/robot_arm/src/test_controller.py
+++ src/robot_arm/src/test_controller.py
@@ -1,55 +1,83 @@
 #!/usr/bin/env python
-# license removed for brevity
-import rospy
-from std_msgs.msg import String
+
+import sys, yaml
+import rospy, rosbag
+from sensor_msgs.msg import JointState
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from control_msgs.msg import JointTrajectoryControllerState
-from control_msgs.msg import FollowJointTrajectoryActionGoal
-from trajectory_msgs.msg import JointTrajectoryPoint
 import math
-import numpy as np
+  
+def get_cur_pos():
+  try:
+    value = rospy.wait_for_message("/robot_arm/state", JointTrajectoryControllerState, 5.0)
+    state = JointState()
+    state.position = value.actual.positions
+    return state
+  except (rospy.ROSException, rospy.ROSInterruptException):
+    rospy.logerr('Unable to read current position')
+    raise
 
+def wait_for_subs(pub, num_subs, min_time, timeout):
+  end = rospy.Time.now() + rospy.Duration(timeout)
+  rospy.sleep(min_time)
 
-def talker():
-    rospy.loginfo("starting the talker node")
-    rospy.init_node("talker", anonymous=True)
-    rate = rospy.Rate(10)  # 10hz
+  r = rospy.Rate(10)  # check at 10Hz
+  while (pub.get_num_connections() < num_subs) and (rospy.Time.now() < end) and not rospy.is_shutdown():
+    r.sleep()
 
-    pub = rospy.Publisher(
-        "/robot_arm/follow_joint_trajectory/goal",
-        FollowJointTrajectoryActionGoal,
-        queue_size=10,
-    )
+  return (pub.get_num_connections() >= num_subs)
 
-    command = FollowJointTrajectoryActionGoal()
-    command.header.stamp = rospy.Time.now()
-    command.goal.trajectory.joint_names = [
+def get_joint_names():
+
+  default_names = [
         "arm_base_to_base",
         "arm_joint_01",
         "arm_joint_02",
         "wrist_joint_01",
         "wrist_joint_04",
-    ]
-    point_1 = JointTrajectoryPoint()
-    point_1.positions = [0, 0, 0, 0, 0]
-    point_1.velocities = [0, 0, 1.5, 0, 0]
-    point_1.time_from_start = rospy.Duration(5)
-    command.goal.trajectory.points.append(point_1)
+  ]
 
-    point_2 = JointTrajectoryPoint()
-    point_2.positions = [0, 0, math.pi / 2, 0, 0]
-    point_2.velocities = [0, 0, 1.5, 0, 0]
-    point_2.time_from_start = rospy.Duration(10)
-    command.goal.trajectory.points.append(point_2)
+  return default_names
 
-    pub.publish(command)
-    rospy.loginfo("=====send command %r", command.goal.trajectory)
+def test_move_robot():
+  joint_names = get_joint_names()
+  
+  start = get_cur_pos()
+  start_pt = JointTrajectoryPoint()
+  start_pt.positions = [0, 0, 0, 0, 0]
+  start_pt.velocities = [0]*len(start.position)
+  start_pt.time_from_start = rospy.Duration(0.0)
 
-    rate.sleep()
+  point_1 = JointTrajectoryPoint()
+  point_1.positions = [math.pi / 4, 1, 0, 0, 0]
+  point_1.velocities = [0, 0, 0, 0, 0]
+  point_1.time_from_start = rospy.Duration(3.0)
 
+  point_2 = JointTrajectoryPoint()
+  point_2.positions = [math.pi / 4, 1, 1, 0, 0]
+  point_2.velocities = [0, 0, 0, 0, 0]
+  point_2.time_from_start = rospy.Duration(6.0)
+
+  point_3 = JointTrajectoryPoint()
+  point_3.positions = [0, 0, 0, 0, 0]
+  point_3.velocities = [0, 0, 0, 0, 0]
+  point_3.time_from_start = rospy.Duration(9.0)
+
+  traj =  JointTrajectory(joint_names=joint_names, points=[start_pt, point_1, point_2, point_3])
+  pub = rospy.Publisher('/robot_arm/command', JointTrajectory, queue_size=10)
+  if not wait_for_subs(pub, 1, 0.5, 2.0):
+    rospy.logwarn('Timeout while waiting for subscribers.  Publishing trajectory anyway.')
+
+  pub.publish(traj)
+
+def main():
+  rospy.init_node('move_to_joint')
+
+  try:
+    test_move_robot()
+  except:
+    rospy.logerr('Unable to move to commanded position. Aborting.')
+    raise  
 
 if __name__ == "__main__":
-    try:
-        talker()
-
-    except rospy.ROSInterruptException:
-        pass
+  main()
